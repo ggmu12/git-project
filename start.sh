3f5acd59e2ed7aea9dfea6d7a4b06e90d555f0d2
ggmu123 <113854395+ggmu123@users.noreply.github.com>
2022-09-18 22:46:57 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,6 @@ GH_OWNER=ggmu12
 GH_REPOSITORY=git-project
 GH_TOKEN=A3EUPOYQGRXA6OJAQ7SKPQTDE7LFO
 
-
-#REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
-
 cd /home/docker/actions-runner
 
 ./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${GH_TOKEN}
